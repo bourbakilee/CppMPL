d31209f86f0660715379b81f36c89a01be12da5d
LI Yunsheng <bourbaki.lee@gmail.com>
2015-12-20 08:50:12 +0100

    static sqlite3 library

--- mexSpiral3/mexSpiral3.cpp
+++ mexSpiral3/mexSpiral3.cpp
@@ -3,9 +3,9 @@
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	if (nrhs != 2)//
+	if (nrhs != 1)//
 	{
-		mexErrMsgTxt("the number of inputs is must 2");
+		mexErrMsgTxt("the number of inputs is must 1");
 	}
 	if (nlhs != 1)
 	{
@@ -13,9 +13,23 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	}
 
 	//----------------Inputs-------------------
-	double *qs, *qg;
-	qs = mxGetPr(prhs[0]); //q_s (x,y,theta,k)
-	qg = mxGetPr(prhs[1]); //q_g
+	//double *qs, *qg;
+	//qs = mxGetPr(prhs[0]); //q_s (x,y,theta,k)
+	//qg = mxGetPr(prhs[1]); //q_g
+	/*
+	double *x0, *y0, *theta0, *k0, *x1, *y1, *theta1, *k1;
+	x0 = mxGetPr(prhs[0]);
+	y0 = mxGetPr(prhs[1]);
+	theta0 = mxGetPr(prhs[2]);
+	k0 = mxGetPr(prhs[3]);
+	x1 = mxGetPr(prhs[0]);
+	y1 = mxGetPr(prhs[1]);
+	theta1 = mxGetPr(prhs[2]);
+	k1 = mxGetPr(prhs[3]);
+	*/
+	double *q = mxGetPr(prhs[0]);
+	double qs[] = { q[0],q[1],q[2],q[3] };
+	double qg[] = { q[4],q[5],q[6],q[7] };
 	//---------------End of Inputs--------------
 
 	//---------------Open DataBase--------------
